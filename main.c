9d0f857f3aad03bbcedace79f27460fe43c2aacb
srivastavaharsh112 <44082661+srivastavaharsh112@users.noreply.github.com>
2018-10-13 20:05:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+/*int main()
+{ float f,c;
+  scanf("%f",&f);
+  c=(5/9.0)*(f-32);
+  printf("The temperature in celsius is %f",c);
+  return 0;
+}
+
+
+int main()
+{
+    float r,c,a;
+    scanf("%f",&r);
+    a=(22/7.0)*r*r;
+    c=2*(22/7.0)*r;
+    printf("Circumference is %f and Area is %f",c,a);
+}
+
+
+int main()
+{
+    int a,b,c;
+    float avg;
+    scanf("%d %d %d",&a,&b,&c);
+    avg=(a+b+c)/3.0;
+    printf("The average of %d,%d and %d is %f",a,b,c,avg);
+}
+
+
+int main()
+{
+    float p,r,si,amt;
+    int t;
+    scanf("%f %f %d",&p,&r,&t);
+    si=(p*r*t)/100.0;
+    amt=p+si;
+    printf("The simple interest and amount is %f and %f",si,amt);
+}
+
+
+int main()
+{
+    float a,b,c,area,s;
+    scanf("%f %f %f",&a,&b,&c);
+    s=(a+b+c)/2.0;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("The area of triangle is %f",area);
+}
+
+
+int main()
+{
+    float v,c;
+    scanf("%f",&v);
+    c=v*(5/18.0);
+    printf("The velocity in m/sec is %f",c);
+    
+}
+
+
+int main()
+{
+    float u,a,v,t;
+    scanf("%f %f %f",&u,&a,&t);
+    v=u+(a*t);
+    printf("The final velocity is %f",v);
+}
+
+
+int main()
+{
+    float age;
+    scanf("%f",&age);
+    float d;
+    d=3.156*pow(10,7)*age;
+    printf("The age in seconds %f",d);
+}
+*/
